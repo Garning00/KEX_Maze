772eff12a28fd598ff8145e0864e53a9885354c8
lwretman <lwretman@kth.se>
2025-04-15 18:03:58 +0200

    ColorDetection.py added referenceLength outside function calcPx2mmConversion to be adjustable
    main.py Added serialQuery to send converted pos in mm to arduino

--- ColorDetection.py
+++ ColorDetection.py
@@ -1,5 +1,6 @@
 """" Detect color markers for px-to-unit conversion (possible future homography aswell)"""
-""""Source: https://agneya.medium.com/color-detection-using-python-and-opencv-8305c29d4a42"""
+""" Returns relation from pixel to real world unit distance """
+""""Source color detection: https://agneya.medium.com/color-detection-using-python-and-opencv-8305c29d4a42"""
 
 import numpy as np
 import cv2
@@ -9,11 +10,6 @@ def detectionLoop(webcam):
 
     # Convert BGR to HSV colorspace
     hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV)
-
-    # green color
-    green_lower = np.array([40, 25, 25], np.uint8)
-    green_upper = np.array([80, 255, 255], np.uint8)
-    green_mask = cv2.inRange(hsvFrame, green_lower, green_upper)
 
     # blue color
     blue_lower = np.array([100, 90, 100], np.uint8)
@@ -25,34 +21,10 @@ def detectionLoop(webcam):
     # to detect only that particular color
     kernal = np.ones((5, 5), "uint8")
 
-    # green color
-    green_mask = cv2.dilate(green_mask, kernal)
-    res_green = cv2.bitwise_and(imageFrame, imageFrame, mask=green_mask)
-    cv2.imshow("Res_green", res_green)
-
     # blue color
     blue_mask = cv2.dilate(blue_mask, kernal)
     res_blue = cv2.bitwise_and(imageFrame, imageFrame, mask=blue_mask)
     cv2.imshow("Res_blue", res_blue)
-
-    # Creating contour to track green color
-    contours, hierarchy = cv2.findContours(green_mask,
-                                           cv2.RETR_TREE,
-                                           cv2.CHAIN_APPROX_SIMPLE)
-
-    for pic, contour in enumerate(contours):
-        area = cv2.contourArea(contour)
-        if (area > 6500 and area < 7000):
-            x, y, w, h = cv2.boundingRect(contour)
-            imageFrame = cv2.rectangle(imageFrame, (x, y),
-                                       (x + w, y + h),
-                                       (0, 255, 0), 2)
-            center = (x+round(w/2),y+round(h/2))
-            cv2.circle(imageFrame, center, 2, (127, 0, 0), 2)
-
-            cv2.putText(imageFrame, "Green Colour", (x, y),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        1.0, (0, 255, 0))
 
     # Creating contour to track blue color
     contours, hierarchy = cv2.findContours(blue_mask,
@@ -82,6 +54,19 @@ def detectionLoop(webcam):
     cv2.imshow("Color Detection", imageFrame)
     return markerCenters
 
+def calcPx2mmConversion(markers, upperSideLength_mm):
+    # Order markers
+    markers = sorted(markers, key=lambda x: (x[0], x[1]))
+    #print("Markers sorted: ")
+    #print(markers)
+    # sideLengths = np.linalg.norm(markers[0]-markers[1]) # for now only upper side
+    upperSideLength_px = np.sqrt(
+        np.power((markers[1][0] - markers[0][0]), 2) + np.power((markers[1][1] - markers[0][1]), 2))
+    #print(upperSideLength_px)
+    px2mm = upperSideLength_px / upperSideLength_mm
+    #print(px2mm)
+    return px2mm
+
 if __name__ == '__main__':
     # turn on cam
     deviceID = "/dev/video2"
@@ -97,14 +82,7 @@ if __name__ == '__main__':
             cv2.destroyAllWindows()
             break
 
-    # Calculate lengths between markers
-    # Order markers
-    print("Markers sorted: ")
-    markers = sorted(markers, key=lambda x: (x[0], x[1]))
-    print(markers)
-    #sideLengths = np.linalg.norm(markers[0]-markers[1]) # for now only upper side
-    upperSideLength_px = np.sqrt(np.power((markers[1][0]-markers[0][0]),2) + np.power((markers[1][1]-markers[0][1]),2))
-    print(upperSideLength_px)
+    # Calculate lengths between markers (Only upper side length)
     upperSideLength_mm = 290
-    px2mm = upperSideLength_px/upperSideLength_mm
+    px2mm = calcPx2mmConversion(markers,upperSideLength_mm)
     print(px2mm)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 """" import relevant scripts and run here """
 import cv2
 from BallVelocity import GetVelocity_ImageCoords
+import serial
+from time import sleep
 
 def prepareImage(img):
     scale = 0.2
@@ -8,16 +10,29 @@ def prepareImage(img):
     cv2.imshow("Source", img)
 
     # Crop
-    img = img[:, 25:117]
+    #img = img[:, 25:117]
 
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return img
 
+# Query has to follow correct message layout to avoid delay ()
+def serialQuery(message):
+    message += '\n'
+    ser.write(message.encode('ascii'))
+
 if __name__ == '__main__':
+    # Initialize serial
+    serialName: str = '/dev/ttyACM0'  # Linux port name
+    baud: int = 19200
+    ser = serial.Serial(serialName, baudrate=baud)  # , rtscts=False, dsrdtr=False)  # open serial port
+    sleep(1)
+
+    # Import video and parameters
     videoPath = "KEX_Bilder/Top-Toy Labyrint/Video_Flash02.mp4"
     videoPath = "KEX_Bilder/BRIO Labyrint/Video_Lvl3.avi"
     deviceID = "/dev/video2"
     framerate = 30
+    px2mm = 1.5 # Convertion rate from pixles to real world units (gathered from ColorDetection.py)
 
     cap = cv2.VideoCapture(deviceID) # Choose videoPath or DeviceID
     cap.set(3, 640)     # Width
@@ -45,6 +60,14 @@ if __name__ == '__main__':
         #     print("TypeError")
         # except SystemExit as e:
         #     print("SystemExit raised: " + str(e.args[0]))
+
+        # Convert ImgCoords and velocity to IRL units
+        vel = lastValidImgCoords*px2mm
+        pos = lastValidImgCoords*px2mm
+        print(f"V: {vel}mm/s Pos: {pos}mm")
+
+        # Send position to arduino via serial
+        serialQuery(f"{pos[0]} {pos[1]}")
 
         cv2.imshow("Video", imgCurrent)
 
