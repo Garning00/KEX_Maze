3c88dfe9e05f8d42c1bdb58537929cd2b96c37cc
lwretman <lwretman@kth.se>
2025-04-29 20:38:29 +0200

    Changed to half-step micro stepping mode and adjusted PID constants (still needs adjustment). Added stepMode variable to have variables programmed in full step values and then convert.

--- Arduino Sketches/Follow_point/Follow_point.ino
+++ Arduino Sketches/Follow_point/Follow_point.ino
@@ -25,15 +25,30 @@
 
 double SetpointX, InputX, OutputX;
 double SetpointY, InputY, OutputY;
-double KpX = 0.5, KiX = 0.5, KdX = 0.5; 
-double KpY = 0.5, KiY = 0.5, KdY = 0.5; 
-double conversion = 1.8;
 
-int Ts = 15;
+// Constants V1
+//double KpX = 0.07, KiX = 0.1, KdX = 0.05;
+//double KpY = 0.13, KiY = 0.03, KdY = 0.045;
+//double KpY = 0.07, KiY = 0.03, KdY = 0.065;
+
+// Constants V2
+double KpX = 0.008, KiX = 0.01, KdX = 0.01;
+double KpY = 0.007, KiY = 0.01, KdY = 0.01;
+//double KpX = 0.0008, KiX = 0, KdX = 0;
+//double KpY = 0.0001, KiY = 0, KdY = 0;
+
+double conversion = 1.8;
+int stepMode = 2;
+
+int Ts = 10;
 
 int InputXfiltered, InputYfiltered;
 
-int X_current = 513, Y_current = 357;
+// Center Point
+int X_desired = 539, Y_desired = 405;
+
+// Initialize variables
+int X_current = X_desired, Y_current = Y_desired;
 
 
 PID myPIDX(&InputX, &OutputX, &SetpointX, KpX, KiX, KdX, DIRECT);
@@ -49,16 +64,16 @@ void setupMicrostepping() {
   pinMode(MS3_1, OUTPUT);
 
   digitalWrite(MS1_1, HIGH);
-  digitalWrite(MS2_1, HIGH);
-  digitalWrite(MS3_1, HIGH);
+  digitalWrite(MS2_1, LOW);
+  digitalWrite(MS3_1, LOW);
 
   pinMode(MS1_2, OUTPUT);
   pinMode(MS2_2, OUTPUT);
   pinMode(MS3_2, OUTPUT);
 
   digitalWrite(MS1_2, HIGH);
-  digitalWrite(MS2_2, HIGH);
-  digitalWrite(MS3_2, HIGH);
+  digitalWrite(MS2_2, LOW);
+  digitalWrite(MS3_2, LOW);
 }
 
 
@@ -77,30 +92,31 @@ void setup() {
 
   pinMode(ENABLE_PIN_2, OUTPUT);
   digitalWrite(ENABLE_PIN_2, LOW);
-  
-  Serial.setTimeout(2); 
-  Serial.begin(19200); 
+
+  Serial.begin(19200);
+  Serial.flush();
 
   // Initialize position
   resetPosition();
 
   // Set the maximum speed in steps per second:
-  stepper1.setMaxSpeed(3200);
-  stepper2.setMaxSpeed(3200);
+  stepper1.setMaxSpeed(200*stepMode);
+  stepper2.setMaxSpeed(200*stepMode);
   //stepper1.setSpeed(200);
-  stepper1.setAcceleration(1000);
-  stepper2.setAcceleration(1000);
+  stepper1.setAcceleration(500*stepMode); // 1000
+  stepper2.setAcceleration(500*stepMode);
 
-  InputX = 513;
-  InputY = 357;
-  SetpointX = 513; // Center of plane
-  SetpointY = 357; // 
-  
+ // Initialize variables
+  //InputX = X_desired;
+  //InputY = Y_desired;
+  SetpointX = X_desired;  // Center of plane
+  SetpointY = Y_desired;  //
+
 
   myPIDY.SetMode(AUTOMATIC);
   myPIDX.SetMode(AUTOMATIC);
-  myPIDX.SetOutputLimits(-310,310); // (-6, +7) TODO: Convert to steps?
-  myPIDY.SetOutputLimits(-500,500);// (-10, +8) 
+  myPIDX.SetOutputLimits(-35*stepMode,35*stepMode); // (-6, +7) TODO: Convert to steps?
+  myPIDY.SetOutputLimits(-15*stepMode,15*stepMode);// (-10, +8)
   myPIDX.SetSampleTime(Ts);
   myPIDY.SetSampleTime(Ts);
 }
@@ -116,16 +132,14 @@ void loop() {
         SetpointX = X_desired;
         SetpointY = Y_desired;
       }
-      
-     while (Serial.available() > 0){
+
+    InputX = X_current;
+    InputY = Y_current;
+
+    while (Serial.available() > 0){
         Serial.read();
       }
     }
-  }
-
-  InputX = X_current;
-  InputY = Y_current;
-  
 
   myPIDX.Compute();
   myPIDY.Compute();
@@ -139,5 +153,5 @@ void loop() {
   stepper1.run();
   stepper2.run();
 
-  
+
 }
