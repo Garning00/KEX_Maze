f1a5169e5fc35e83e509f46875723a4c66d3e07f
lwretman <lwretman@kth.se>
2025-05-02 22:30:44 +0200

    Brought back ability to move with keyboard.

--- StepperMovement.py
+++ StepperMovement.py
@@ -8,51 +8,50 @@ def move(message):
     ser.write(message.encode('ascii'))
     #ser.flush()
 
-serialName: str = "COM5" # Windows port name
-#   serialName: str = '/dev/ttyACM0' #Linux port name
+#serialName: str = "COM5" # Windows port name
+serialName: str = '/dev/ttyACM0' #Linux port name
 baud: int = 19200
 ser = serial.Serial(serialName, baudrate=baud)#, rtscts=False, dsrdtr=False)  # open serial port
 sleep(1)
 
 if __name__ == '__main__':
-     while(True):
-          message = input()
-          if message == 'stop':
-             break
-          move(message)
+    while (True):
+        message = input()
+        if message == 'stop':
+            break
+        move(message)
 
     #region Keyboard
     # Keyboard måste köras som root: sudo pycharm-community
     # Alla libraries måste installeras på root python också
-    #Mstep1: int = 0
-    #Mstep2: int = 0
+    Mstep1: int = 0
+    Mstep2: int = 0
 
-    #def on_key_event(event):
-        #global Mstep1
-        #global Mstep2
-        #if event.event_type == "down":
+    def on_key_event(event):
+        global Mstep1
+        global Mstep2
+        if event.event_type == "down":
             #print(f"Key {event.name} pressed")
-            #if event.name not in ["up","down","right","left"]:
-                #print("not move key")
+            if event.name not in ["up","down","right","left"]:
+                print("not move key")
                 #return
-            #if event.name == "up":
-                #Mstep1 += 1
-            #if event.name == "down":
-                #Mstep1 -= 1
-            #if event.name == "right":
-                #Mstep2 += 1
-            #if event.name == "left":
-                #Mstep2 -= 1
+            if event.name == "up":
+                Mstep1 += 1
+            if event.name == "down":
+                Mstep1 -= 1
+            if event.name == "right":
+                Mstep2 += 1
+            if event.name == "left":
+                Mstep2 -= 1
 
             #print(f"{Mstep1} {Mstep2} 0")
-            #move(f"{Mstep1} {Mstep2} 0")
+            move(f"{Mstep1} {Mstep2} 0")
 
-    #keyboard.hook(on_key_event)
+    keyboard.hook(on_key_event)
     # This will block the script and wait until 'esc' is pressed
-    #keyboard.wait("esc")
+    keyboard.wait("esc")
 
     #endregion
-
 
 # funktion vinklar till rörelse
 # Hur snabbt kan man skicka pos utan att det ballar ur?
