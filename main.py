4547202988dd43f9e546cbdec5d70731f15be309
lwretman <lwretman@kth.se>
2025-05-05 21:19:25 +0200

    Replaced mouseX,mouseY variable with mouseSetpoint Added getCurrentSetpoint that can differentiate setpointMode (either "mouse" or "list") Added ability to paste in list of points to follow (ex paste in from Route_wFocus.py)

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 """" import relevant scripts and run here """
-import time
-
 """ sudo chmod a+rw /dev/ttyACM0 """
 import cv2
 from BallVelocity import GetVelocity_ImageCoords
 import serial
-from time import sleep
+import time
 from matplotlib import pyplot
 import numpy as np
+from Timer import Timer
 
 def prepareImage(img):
     img = prepareImageScaleCrop(img)
@@ -35,10 +34,20 @@ def serialQuery(message):
     #print("message sent at" + time.ctime())
 
 def getMouseClick(event,x,y,flags,param):
-    global mouseX,mouseY
+    global mouseSetpoint
     if event == cv2.EVENT_LBUTTONDOWN:
-        mouseX,mouseY = x,y
+        mouseSetpoint = (x,y)
         print(f"mouse click at ({x},{y})")
+
+def getCurrentSetpoint():
+    global pointIndex
+    if setpointMode == "mouse":
+        return mouseSetpoint
+    elif setpointMode == "list":
+        if pointIndex < len(pointList):
+            return pointList[pointIndex]
+        else:
+            return None  # or hold last point
 
 def draw_cross(img, center, size, color, thickness):
     x, y = center
@@ -52,19 +61,28 @@ if __name__ == '__main__':
     serialName: str = '/dev/ttyACM0'  # Linux port name
     baud: int = 19200
     ser = serial.Serial(serialName, baudrate=baud)  # , rtscts=False, dsrdtr=False)  # open serial port
-    sleep(1)
+    time.sleep(1)
 
     # Import video and parameters
     videoPath = "KEX_Bilder/Top-Toy Labyrint/Video_Flash02.mp4"
     videoPath = "KEX_Bilder/BRIO Labyrint/Video_Lvl3.avi"
     #deviceID = 0
-    deviceID = "/dev/video2"
+    deviceID = "/dev/video0"
     framerate = 30
     px2mm = 0.872441051775 # Convertion rate from pixles to real world units (gathered from ColorDetection.py)
     frame_width = 640
     frame_height = 480
-    scale = 0.5
+    scale = 0.5 # !! Not global variable, must be set to same as in prepareImageScaleCrop()
     vidResultSavePath = "KEX_Bilder/BRIO Labyrint/Results/"
+
+    pointIndex = 0
+    pointList = [(126, 205), (214, 198), (232, 185), (264, 177), (253, 132), (267, 93), (262, 68), (249, 35), (194, 24), (209, 56), (190, 85), (167, 97), (147, 120), (180, 160), (145, 172), (112, 163), (47, 188), (50, 154), (67, 131), (108, 130), (123, 102), (103, 82), (72, 79), (48, 79), (61, 84), (91, 79), (101, 84), (62, 86), (40, 78), (41, 75), (46, 38)]
+    mouseSetpoint = (160,25)
+    setpointMode = "mouse" #"mouse" or "list"
+
+    errorTolerance = 10 # [px]
+    timeAtPoint = 2000 # [ms]
+    errorTimer = Timer(1.0)
 
     cap = cv2.VideoCapture(deviceID) # Choose videoPath or DeviceID
     cap.set(3, frame_width)     # Width
@@ -78,8 +96,6 @@ if __name__ == '__main__':
     imgPosPlot = imgPast # Save gray img instead
 
     # Save video
-    #size = (frame_width,frame_height)
-    #size = [int(e / 2) for e in size]
     vidResult = cv2.VideoWriter(f'{vidResultSavePath}Run@{time.ctime()}.avi',
                          cv2.VideoWriter_fourcc(*'MJPG'),
                          framerate, (int(frame_width*scale),int(frame_height*scale)), isColor=False) # !!! To get output you need to release, have correct dimensions and set isColor correctly
@@ -88,9 +104,6 @@ if __name__ == '__main__':
 
     lastValidImgCoords = [0,0]#None
     lastValidImgVelocity = [0, 0]
-
-    mouseX, mouseY = 160,25
-
 
     # Initialize all positions for plotting
     savedPosx = []
@@ -110,19 +123,10 @@ if __name__ == '__main__':
 
         lastValidImgVelocity, lastValidImgCoords = GetVelocity_ImageCoords(imgPast, imgCurrent, framerate, lastValidImgCoords, lastValidImgVelocity)
         #print(f"LastVel: {lastValidImgVelocity} Last Coords: {lastValidImgCoords}")
-        # try:
-        #     velImgCoords = GetVelocity_ImageCoords(imgPast,imgCurrent,60)
-        #     #print(GetVelocity_ImageCoords(imgPast,imgCurrent,60))
-        # except TypeError:
-        #     print("TypeError")
-        # except SystemExit as e:
-        #     print("SystemExit raised: " + str(e.args[0]))
 
         # Convert ImgCoords and velocity to IRL units
-        #vel = lastValidImgVelocity*px2mm
-        #print(f"PosPX: {lastValidImgCoords}px")
         vel = [e * px2mm for e in lastValidImgVelocity]
-        #pos = lastValidImgCoords*px2mm
+        # print(f"PosPX: {lastValidImgCoords}px")
         pos = [e * px2mm for e in lastValidImgCoords]
         #print(f"V: {vel}mm/s Pos: {pos}mm")
         #print(f"PosMM: {pos}mm")
@@ -130,12 +134,34 @@ if __name__ == '__main__':
         cv2.imshow("Video", imgCurrent)
         cv2.setMouseCallback("Video", getMouseClick)
 
-        #print(f"mouse: {mouseX},{mouseY} and mm {mouseX*px2mm},{mouseY*px2mm}")
+        # Update Setpoint
+        setPoint = getCurrentSetpoint()
+
+        print(f"setPoint: {np.array(setPoint)}")
+        print(f"Current pos: {lastValidImgCoords[:2]}")
+
+        # # Increase pointIndex if ball have been stable around setpoint for 1s
+        if (setpointMode == "list"):
+            errorDistance = np.linalg.norm(np.array(lastValidImgCoords[:2]) - np.array(setPoint))
+            if (errorDistance > errorTolerance):
+                errorTimer.stop()
+            else:
+                print("Inside tolerance - Starting timer!")
+                errorTimer.start() # Doesn't restart when calling .start()
+                if errorTimer.is_done():
+                    pointIndex += 1
+                    print(f"Held position for 1 sec! Moving to: {pointList[pointIndex]}")
+                    errorTimer.stop()
+
+
+        #print(f"mouse: {mouseSetpoint[0]},{mouseSetpoint[1]} and mm {mouseSetpoint[0]*px2mm},{mouseSetpoint[1]*px2mm}")
+        #print(f"Setpoint: {setPoint}")
+
         # Send position to arduino via serial
         # in mm:s
-        #serialQuery(f"{round(pos[0],8)} {round(pos[1],8)} {round(mouseX*px2mm,8)} {round(mouseY*px2mm,8)}")
+        #serialQuery(f"{round(pos[0],8)} {round(pos[1],8)} {round(mouseSetpoint[0]*px2mm,8)} {round(mouseSetpoint[1]*px2mm,8)}")
         # in px:s
-        serialQuery(f"{lastValidImgCoords[0]} {lastValidImgCoords[1]} {mouseX} {mouseY}")
+        serialQuery(f"{lastValidImgCoords[0]} {lastValidImgCoords[1]} {setPoint[0]} {setPoint[1]}")
 
         # baud needs to account for 4+4+4+4 chars = 16
         # 1/baudrate * chars * 1000 = ms to send message
@@ -144,8 +170,8 @@ if __name__ == '__main__':
 
         savedPosx.append(lastValidImgCoords[0])
         savedPosy.append(lastValidImgCoords[1])
-        savedRefx.append(mouseX)
-        savedRefy.append(mouseY)
+        savedRefx.append(setPoint[0])
+        savedRefy.append(setPoint[1])
         savedVelx.append(vel[0])
         savedVely.append(vel[1])
 
